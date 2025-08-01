af5bc39547adead20859d2e0335569bc4aa499a7
Dotnetdeveloper <dotnetdeveloper20xx@hotmail.com>
2025-08-01 18:22:16 +0100

    add Swagger setup with XML comments and full API documentation support

--- StoreBoost/StoreBoost.Api/Program.cs
+++ StoreBoost/StoreBoost.Api/Program.cs
@@ -1,35 +1,47 @@
 using StoreBoost.Api.Middleware;
 using StoreBoost.Persistence;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure();
 builder.Services.AddPersistence();
 
-
-
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+
+//  Swagger setup with XML comments support
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "StoreBoost API",
+        Version = "v1",
+        Description = "Appointment booking system with Clean Architecture and CQRS"
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
+//  Global exception handler
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//  Swagger middleware (enabled in all environments)
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.MapOpenApi();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "StoreBoost API v1");
+    c.RoutePrefix = string.Empty; // Makes Swagger UI available at root (/)
+});
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
